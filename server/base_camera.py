b88414b9fc0559b2c60f6ca209703c808c313e43
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2025-02-16 14:58:41 +0100

    Fix formatting issues in base_camera.py by adjusting import order

--- server/base_camera.py
+++ server/base_camera.py
@@ -1,6 +1,6 @@
-import time
 import threading
-import cv2
+import time
+
 try:
     from greenlet import getcurrent as get_ident
 except ImportError:
@@ -14,6 +14,7 @@ class CameraEvent(object):
     """An Event-like class that signals all active clients when a new frame is
     available.
     """
+
     def __init__(self):
         self.events = {}
 
