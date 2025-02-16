9722bb33871a9e42ea087702b0ef8bbdbf0d3102
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2025-02-16 15:15:41 +0100

    Refactor switch.py for improved code consistency and readability

--- server/switch.py
+++ server/switch.py
@@ -6,8 +6,10 @@
 # Author      : William
 # Date        : 2018/08/22
 
-import RPi.GPIO as GPIO
 import time
+
+import RPi.GPIO as GPIO
+
 
 def switchSetup():
     GPIO.setwarnings(False)
@@ -16,48 +18,50 @@ def switchSetup():
     GPIO.setup(6, GPIO.OUT)
     GPIO.setup(13, GPIO.OUT)
 
+
 def switch(port, status):
     if port == 1:
         if status == 1:
             GPIO.output(5, GPIO.HIGH)
         elif status == 0:
-            GPIO.output(5,GPIO.LOW)
+            GPIO.output(5, GPIO.LOW)
         else:
             pass
     elif port == 2:
         if status == 1:
             GPIO.output(6, GPIO.HIGH)
         elif status == 0:
-            GPIO.output(6,GPIO.LOW)
+            GPIO.output(6, GPIO.LOW)
         else:
             pass
     elif port == 3:
         if status == 1:
             GPIO.output(13, GPIO.HIGH)
         elif status == 0:
-            GPIO.output(13,GPIO.LOW)
+            GPIO.output(13, GPIO.LOW)
         else:
             pass
     else:
         print('Wrong Command: Example--switch(3, 1)->to switch on port3')
 
+
 def set_all_switch_off():
-    switch(1,0)
-    switch(2,0)
-    switch(3,0)
+    switch(1, 0)
+    switch(2, 0)
+    switch(3, 0)
+
 
 if __name__ == '__main__':
     switchSetup()
     try:
         while 1:
-            switch(1,1)
-            switch(2,1)
-            switch(3,1)
-            print("Light on....")
+            switch(1, 1)
+            switch(2, 1)
+            switch(3, 1)
+            print("Light on....")
             time.sleep(1)
             set_all_switch_off()
-            print("Light off....")
+            print("Light off....")
             time.sleep(1)
-    except:
+    except KeyboardInterrupt:
         set_all_switch_off()
-    
