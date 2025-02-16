49380ba61e2d8379cc0a929c7197b5da6852509e
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2025-02-16 15:07:41 +0100

    Refactor PID class to improve structure and add status retrieval methods

--- server/PID.py
+++ server/PID.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 # File name   : car_dir.py
-# Description : By controlling Servo,thecamera can move Up and down,left and right and the Ultrasonic wave can move to left and right.
+# Description : By controlling Servo,the camera can move Up and down,
+#               left and right and the Ultrasonic wave can <- & ->.
 # Website     : www.adeept.com
 # E-mail      : support@adeept.com
 # Author      : William
@@ -11,20 +12,53 @@ import time
 class PID:
     def __init__(self):
         self.Kp = 0
-        self.Kd = 0
         self.Ki = 0
-        self.Initialize()
+        self.Kd = 0
 
-    def SetKp(self,invar):
+        self.windup_guard = 20.0
+        self.sample_time = 0.01
+
+        self.current_time = time.time()
+        self.last_time = self.current_time
+
+        self.PTerm = 0.0
+        self.ITerm = 0.0
+        self.DTerm = 0.0
+        self.last_error = 0.0
+        self.last_output = 0.0
+
+        self.output = 0.0
+
+    def get_status(self):
+        """Get current PID controller status"""
+        return {
+            'P': self.PTerm,
+            'I': self.ITerm,
+            'D': self.DTerm,
+            'output': self.output,
+            'error': self.last_error,
+            'settings': {
+                'Kp': self.Kp,
+                'Ki': self.Ki,
+                'Kd': self.Kd,
+                'windup_guard': self.windup_guard
+            }
+        }
+
+    def SetWindup(self, windup):
+        """Set the windup guard to prevent integral term from growing"""
+        self.windup_guard = windup
+
+    def SetKp(self, invar):
         self.Kp = invar
 
-    def SetKi(self,invar):
+    def SetKi(self, invar):
         self.Ki = invar
 
-    def SetKd(self,invar):
+    def SetKd(self, invar):
         self.Kd = invar
 
-    def SetPrevError(self,preverror):
+    def SetPrevError(self, preverror):
         self.prev_error = preverror
 
     def Initialize(self):
@@ -37,22 +71,24 @@ class PID:
         self.Ci = 0
         self.Cd = 0
 
-    def GenOut(self,error):
+    def GenOut(self, error):
         self.currtime = time.time()
         dt = self.currtime - self.prevtime
         de = error - self.prev_error
 
-        self.Cp = self.Kp*error
-        self.Ci += error*dt
+        self.Cp = self.Kp * error
+        self.Ci += error * dt
 
         self.Cd = 0
         if dt > 0:
-            self.Cd = de/dt
+            self.Cd = de / dt
 
         self.prevtime = self.currtime
         self.prev_error = error
 
-        return self.Cp + (self.Ki*self.Ci) + (self.Kd*self.Cd)
+        return self.Cp + (self.Ki * self.Ci) + (self.Kd * self.Cd)
+
+
 '''
 pid = PID()
 pid.SetKp(Kp)
